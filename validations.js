4d4411bb3979ef9fd3cbc211d3617f1ef0e8d2f8
Ishimwe Pacifique <ishimweinstein@gmail.com>
2024-02-07 17:12:26 +0200

    Web Technology

--- validations.js
+++ validations.js
@@ -9,6 +9,7 @@ function validateRegistrationForm() {
     var address = document.getElementById("address").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
+    const loginBtn = document.getElementById("loginBtn");
 
     // Basic validation example (you can add more complex validations)
     if (firstName === "" || lastName === "" || email === "" || phone === "" || position === "" ||
@@ -25,14 +26,20 @@ function validateRegistrationForm() {
     return true;
 }
 
-function validateLoginForm() {
+loginBtn.onclick = function validateLoginForm() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     // Basic validation example (you can add more complex validations)
     if (username === "" || password === "") {
-        alert("All fields must be filled out");
+        alert("All fields are required!");
         return false;
+    }
+    else if(username ==="admin" && password ==="admin"){
+        alert("Login Success!");
+    }
+    else{
+        alert("Login Failed!");
     }
 
     return true;
